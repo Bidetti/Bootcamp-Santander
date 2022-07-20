67e2f6652ab43c992e7751b54728d17c21d6fd82
Bidetti <rafael.bidetti@gmail.com>
2022-07-20 11:18:42 -0300

    Como importar bibliotecas com typescript

--- Módulo II - HTML-CSS3-JS/Introdução ao TypeScript - Explorando Classes, Tipos e Interfaces/index.ts
+++ Módulo II - HTML-CSS3-JS/Introdução ao TypeScript - Explorando Classes, Tipos e Interfaces/index.ts
@@ -1,30 +1,11 @@
-interface Cachorro {
-    nome: string;
-    idade: number;
-    parqueFavorito?: string;
-}
+export const numero = 2;
 
-type CachorroSomenteLeitura = {
-    +readonly [K in keyof Cachorro]-?: Cachorro[K]
-}
+import $ from 'jquery';
 
-const meuCachorro: Cachorro = {
-    nome: 'Apolo',
-    idade: 14,
-    parqueFavorito: "Parque do Povo"
-}
-
-class meuCachorroC implements CachorroSomenteLeitura {
-    idade;
-    nome;
-    parqueFavorito;
-
-    constructor(nome, idade) {
-        this.idade = idade;
-        this.nome = nome;
+$.fn.extend({
+    novaFuncao() {
+        console.log("Chamou nova funcao")
     }
-}
+})
 
-const cao = new meuCachorroC("Apolo", 14)
-cao.idade = 8;
-console.log(cao)
+$('body').novaFuncao();
