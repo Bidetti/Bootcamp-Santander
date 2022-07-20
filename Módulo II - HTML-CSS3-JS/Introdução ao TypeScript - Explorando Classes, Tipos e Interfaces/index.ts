f069fb08a16356bea8398956df8cd0fd367f3002
Bidetti <36907728+Bidetti@users.noreply.github.com>
2022-07-19 21:33:42 -0300

    Interfaces e Types

--- Módulo II - HTML-CSS3-JS/Introdução ao TypeScript - Explorando Classes, Tipos e Interfaces/index.ts
+++ Módulo II - HTML-CSS3-JS/Introdução ao TypeScript - Explorando Classes, Tipos e Interfaces/index.ts
@@ -1,5 +1,27 @@
-function soma(a: number, b: number){
-    return a + b;
+//types
+
+//interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: `terrestre` | `aquatico`;
+    domestico: boolean;
 }
 
-soma(1, 5)
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal{
+    porte: `pequeno` | `medio` | `grande`
+}
+
+type IDomestico = IFelino | ICanino;
+
+const animal: IDomestico = {
+    domestico: true,
+    nome: `Cachorro`,
+    porte: "medio",
+    tipo: "terrestre"
+}
+
